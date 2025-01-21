dc3adf8abb366956f20bb8e1f9a0b33a514e36d5
Vishal <swamivishal14781516@gmail.com>
2025-01-21 11:48:21 +0530

    starting frontend

---
+++ src/frontend/pages/home.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Home = (props) => {
+    return<>
+    <h1>The Recipe Roster Home Page!</h1>
+    <p>it will be ready soon to be served on your table :p</p>
+    </>
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './frontend/pages/home'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
